6951b464f8fef86bb0bb60a35f008fe5468d1af3
adnier87 <adnier87@gmail.com>
2024-02-08 21:17:21 -0600

    Change approach

--- config/s3-client.config.ts
+++ config/s3-client.config.ts
@@ -1,12 +1,9 @@
 import { S3Client } from '@aws-sdk/client-s3';
+import 'dotenv/config';
 
 // Configure AWS SDK
 const s3 = new S3Client({
   region: process.env.AWS_REGION as string,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
-  }
 })
 
 export default s3;
--- index.ts
+++ index.ts
@@ -1,49 +1,32 @@
 import serverless from 'serverless-http';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import crypto from 'crypto';
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import 'dotenv/config';
 
-import s3 from './config/s3-client.config';
+import s3Client from './config/s3-client.config';
 
 const app = express();
 
-// Initialize multer middleware with multer-s3 storage engine
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: (process.env.AWS_S3_BUCKET_NAME as string),
-    acl: 'private',
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: function (req: Request, file, cb) {
-      crypto.pseudoRandomBytes(16, function (err, raw) {
-        if (err) return cb(err);
-        cb(null, raw.toString('hex') + Date.now().toString() + '_' + file.originalname);
-      });
-    },
-  }),
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB file size limit
-}).single('file');
+app.get('/presigned-url', async (req: Request, res: Response) => {
+  const { key } = req.query;
 
-app.post('/upload', (req: Request, res: Response) => {
-  upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      // A Multer error occurred when uploading.
-      return res.status(500).json({
-        error: err.message,
-      });
-    } else if (err) {
-      // An unknown error occurred when uploading.
-      return res.status(500).json({
-        error: 'An error occurred while uploading the file.',
-      });
-    }
+  if (!key && typeof key !== 'string') {
+    return res.status(400).json({ message: 'A valid \'key\' query parameter is required.' });
+  }
 
-    // Everything went fine, file is successfully uploaded
-    res.status(200).json({
-      message: 'File uploaded successfully',
+  try {console.log(process, process.env.AWS_S3_BUCKET_NAME)
+    const command = new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: key as string,
     });
-  });
+
+    const presignedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 }); // URL expires in 1 hour
+    return res.status(200).json({ presignedUrl });
+  } catch (error) {
+    console.error('Error creating presigned URL', error);
+    return res.status(500).json({ message: 'Error creating presigned URL', error });
+  }
 });
 
 // Ensure the server can handle multiple concurrent requests efficiently
